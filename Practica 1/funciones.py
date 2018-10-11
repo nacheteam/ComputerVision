514f1ded2a1093a123597f771d20b8a7ecea3746
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-10-11 11:27:25 +0200

    Subpart B

--- Practica 1/funciones.py
+++ Practica 1/funciones.py
@@ -2,6 +2,12 @@ import practica0
 import cv2
 
 ################################################################################
+##                             CONSTANTES                                     ##
+################################################################################
+
+KERNEL1D_SIZE = 100
+
+################################################################################
 ####   PUNTO 1: escribir funciones que implementen los siguientes puntos.   ####
 ################################################################################
 
@@ -19,6 +25,20 @@ def gaussianConvolution(sigmaX,sigmaY,hsize,wsize,im):
     practica0.pintaI(smoothed)
 
 
+################################################################################
+## Apartado B: Usar getDerivKernels para obtener las máscaras 1D que permiten ##
+## calcular la convolución 2D con máscaras de derivadas. Representar e        ##
+## interpretar dichas máscaras 1D para distintos valores de sigma.            ##
+################################################################################
+
+def printDerivKernel(sigma,ksize):
+    kernel = cv2.getGaussianKernel(ksize,sigma)
+    practica0.pintaI(kernel)
+
+################################################################################
+##                                 MAIN                                       ##
+################################################################################
+
 def main():
     #Leo la imagen
     img = cv2.imread("../Images/lena.jpg",-1)
@@ -26,5 +46,10 @@ def main():
     print("Ejecutando el apartado A con varios parámetros.")
     for sigmaX,sigmaY,hsize,wsize in zip([0,1,3,5],[0,3,1,5],[1,3,5,7,11],[1,3,7,5,11]):
         gaussianConvolution(sigmaX,sigmaY,hsize,wsize,img)
+
+    #Ejercicio 1 Apartado B
+    print("Ejecutando el apartado B con varios parámetros de sigma. Se pinta con tamaño 100 para poder ver bien el resultado.")
+    for sigma in [0.1,1,2,3,4,5,6,7,8,9,10]:
+        printDerivKernel(sigma,KERNEL1D_SIZE)
 
 main()
